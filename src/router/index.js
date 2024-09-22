5e98183214a4aeb382121ed89c7b1a0145f787d4
chanil <hci970705@gmail.com>
2024-09-22 14:48:20 +0900

    feat: OCR.js 기능 추가 - 한 cell 내 말소된 요소가 2개인 경우까지 해결 - 근저당권변경으로 금액이 변경될 경우 변경 금액 반영

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Map from '../pages/Map.vue';
-import AddProperty from '../pages/AddProperty.vue';
+import AddProperty2 from '../pages/AddProperty2.vue';
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Map',
-      component: Map,
-    },
-    {
-      path: '/add',
-      name: 'AddProperty',
-      component: AddProperty,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Map',
+            component: Map,
+        },
+        {
+            path: '/add',
+            name: 'AddProperty2',
+            component: AddProperty2,
+        },
+    ],
 });
 
 export default router;
