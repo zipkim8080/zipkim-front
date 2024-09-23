8a79c26e5bf15856ab86cf4eeddfc2e16561d082
chanil <hci970705@gmail.com>
2024-09-23 11:37:06 +0900

    feat: Addproperty 병합

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Map from '../pages/Map.vue';
-import AddProperty2 from '../pages/AddProperty2.vue';
+import AddProperty from '../pages/AddProperty.vue';
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -11,8 +11,8 @@ const router = createRouter({
         },
         {
             path: '/add',
-            name: 'AddProperty2',
-            component: AddProperty2,
+            name: 'AddProperty',
+            component: AddProperty,
         },
     ],
 });
