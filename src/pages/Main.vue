30015ce9e5d460f141be5ee2a1772f0e864846b3
chanil <hci970705@gmail.com>
2024-10-01 11:41:34 +0900

    fix: 누락 기능추가, 연결 버그 수정

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -15,9 +15,9 @@ const showModal = ref(false);
 
 // 단순 페이지 이동 //
 const regi = () => {
-  router.push({
-    path: '/add',
-  });
+    router.push({
+        path: '/add',
+    });
 };
 
 // 빈페이지;
@@ -43,73 +43,81 @@ const regi = () => {
 //     trust: true,
 //     auction: false,
 //     loan: 10,
-//     leaseAmount: 10, // 100- 30 50 70
+//     leaseAmount: 1000000000, // 100- 30 50 70
 // };
 
 const handleOcrCompleted = (result) => {
-  ocrData.value = result;
-  showModal.value = false;
+    ocrData.value = result;
+    showModal.value = false;
 };
 </script>
 <template>
-  <div class="search-overlay">
-    <SearchFilter />
-  </div>
-  <LoginButton />
-  <XXDongButton />
-  <!-- 매물 등록 버튼 -->
-  <div class="register-overlay">
-    <input class="kb_btn" type="button" value="매물 등록" @click="regi()" />
-  </div>
-  <!-- 등기 확인 버튼 -->
-  <div class="register-overlay2">
-    <input
-      class="kb_btn"
-      type="button"
-      value="등기 확인"
-      @click="showModal = true"
+    <div class="search-overlay">
+        <SearchFilter />
+    </div>
+    <LoginButton />
+    <XXDongButton />
+    <!-- 매물 등록 버튼 -->
+    <div class="register-overlay">
+        <input class="kb_btn" type="button" value="매물 등록" @click="regi()" />
+    </div>
+    <!-- 등기 확인 버튼 -->
+    <div class="register-overlay2">
+        <input
+            class="kb_btn"
+            type="button"
+            value="등기 확인"
+            @click="showModal = true"
+        />
+    </div>
+
+    <!-- 모달 백드롭 -->
+    <div
+        v-if="showModal"
+        class="modal-backdrop"
+        @click="showModal = false"
+    ></div>
+
+    <!-- CheckMyDoc 모달 -->
+    <CheckMyDoc
+        v-if="showModal"
+        @ocrCompleted="handleOcrCompleted"
+        @close="showModal = false"
     />
-  </div>
 
-  <!-- 모달 백드롭 -->
-  <div v-if="showModal" class="modal-backdrop" @click="showModal = false"></div>
+    <!-- MyDocResultPage 모달 -->
+    <MyDocResultPage
+        v-if="ocrData"
+        :ocrData="ocrData"
+        @close="ocrData = null"
+    />
 
-  <!-- CheckMyDoc 모달 -->
-  <CheckMyDoc
-    v-if="showModal"
-    @ocrCompleted="handleOcrCompleted"
-    @close="showModal = false"
-  />
-
-  <!-- MyDocResultPage 모달 -->
-  <MyDocResultPage v-if="ocrData" :ocrData="ocrData" @close="ocrData = null" />
-
-  <KakaoMap class="kakao-map" />
+    <KakaoMap class="kakao-map" />
 </template>
 
 <style scoped>
 .register-overlay {
-  position: absolute;
-  bottom: 2%;
-  right: 1%;
-  z-index: 9;
+    position: absolute;
+    bottom: 2%;
+    right: 1%;
+    z-index: 9;
 }
 
 .register-overlay2 {
-  position: absolute;
-  bottom: 2%;
-  right: 1%;
-  transform: translateX(-110%);
-  z-index: 9;
+    position: absolute;
+    bottom: 2%;
+    right: 1%;
+    transform: translateX(-110%);
+    z-index: 9;
 }
 
 .modal-backdrop {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.4);
-  z-index: 15;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    z-index: 15;
 }
 </style>
