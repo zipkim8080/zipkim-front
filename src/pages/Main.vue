b6197df34364ff43baa7fc61515790d4f9c3fc32
Kim Jong Yeong <621kkjy@gmail.com>
2024-10-02 16:24:05 +0900

    커밋

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -57,11 +57,8 @@ const handleOcrCompleted = (result) => {
   <div class="search-overlay">
     <SearchFilter />
   </div>
-<<<<<<< HEAD
   <Timer />
-=======
   <PriceToggle />
->>>>>>> c34d97adfb93d270f96e1def40b51eb7fe5d46c0
   <LoginButton />
   <XXDongButton />
   <!-- 매물 등록 버튼 -->
@@ -70,7 +67,6 @@ const handleOcrCompleted = (result) => {
   </div>
   <!-- 등기 확인 버튼 -->
   <div class="register-overlay2">
-<<<<<<< HEAD
     <input class="kb_btn" type="button" value="등기 확인" @click="showModal = true" />
   </div>
 
@@ -83,29 +79,6 @@ const handleOcrCompleted = (result) => {
   <!-- MyDocResultPage 모달 -->
   <MyDocResultPage v-if="ocrData" :ocrData="ocrData" @close="ocrData = null" />
 
-=======
-    <input
-      class="kb_btn"
-      type="button"
-      value="등기 확인"
-      @click="showModal = true"
-    />
-  </div>
-
-  <!-- 모달 백드롭 -->
-  <div v-if="showModal" class="modal-backdrop" @click="showModal = false"></div>
-
-  <!-- CheckMyDoc 모달 -->
-  <CheckMyDoc
-    v-if="showModal"
-    @ocrCompleted="handleOcrCompleted"
-    @close="showModal = false"
-  />
-
-  <!-- MyDocResultPage 모달 -->
-  <MyDocResultPage v-if="ocrData" :ocrData="ocrData" @close="ocrData = null" />
-
->>>>>>> c34d97adfb93d270f96e1def40b51eb7fe5d46c0
   <KakaoMap class="kakao-map" />
 </template>
 
