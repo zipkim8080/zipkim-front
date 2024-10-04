4e693f3eb9fcb54d38922bc3d4c8dd75e010f01e
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-04 11:51:16 +0900

    fix: 이미지사이즈조정

--- src/components/detail/PropertyList.vue
+++ src/components/detail/PropertyList.vue
@@ -39,7 +39,7 @@ const openModal = () => {
       <p>상세주소: {{ property.detailAddress }}</p>
       <p>층: {{ property.floor }}</p>
       <p>매물id: {{ property.id }}</p>
-      <img :src="property.imageUrl" />
+      <img style="width: 200px; height: 130px" :src="property.imageUrl" />
       <div>--------------------------------------</div>
     </div>
     <div v-if="isModalOpen" @click="openModal" @close="isModalOpen = false">
