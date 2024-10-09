2b1412ad4f8ce742aeb43431afbe514731676c23
Oh Seung Hyeok <owin1022@naver.com>
2024-10-09 22:30:50 +0900

    style: 매물 상세보기 화면 스타일 수정
    - 매물 등록 페이지처럼 배경 이중으로해서 이질감을 줄임
    - 매물 상세보기에 즐겨찾기 버튼 들어올 자리와 매물 번호로 구분

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -10,137 +10,137 @@ const emit = defineEmits(['close']);
 const infoModal = ref(false);
 
 const infoModalOpen = () => {
-    infoModal.value = !infoModal.value;
+  infoModal.value = !infoModal.value;
 };
 
 const naverLogin = async () => {
-    window.location.href = 'http://52.79.206.11/oauth2/authorization/naver';
-    // window.location.href = 'http://localhost:8080/oauth2/authorization/naver';
+  window.location.href = 'http://52.79.206.11/oauth2/authorization/naver';
+  // window.location.href = 'http://localhost:8080/oauth2/authorization/naver';
 };
 const googleLogin = () => {
-    window.location.href = 'http://52.79.206.11/oauth2/authorization/google';
-    // window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  window.location.href = 'http://52.79.206.11/oauth2/authorization/google';
+  // window.location.href = 'http://localhost:8080/oauth2/authorization/google';
 };
 const kakaoLogin = () => {
-    window.location.href = 'http://52.79.206.11/oauth2/authorization/kakao';
-    // window.location.href = 'http://localhost:8080/oauth2/authorization/kakao';
+  window.location.href = 'http://52.79.206.11/oauth2/authorization/kakao';
+  // window.location.href = 'http://localhost:8080/oauth2/authorization/kakao';
 };
 
 onMounted(() => {
-    loginStore.loadTokenFromCookies();
+  loginStore.loadTokenFromCookies();
 });
 </script>
 
 <template>
-    <div class="title-box">
-        <div class="title">
-            <h1 class="login-title">로그인</h1>
-            <button class="close-btn" @click="$emit('close')">
-                <i class="fa-solid fa-x"></i>
-            </button>
-        </div>
-        <p class="text">로그인 하면 모든 서비스를 이용할 수 있어요!</p>
+  <div class="title-box">
+    <div class="title">
+      <h1 class="login-title">로그인</h1>
+      <button class="close-btn" @click="$emit('close')">
+        <i class="fa-solid fa-x"></i>
+      </button>
     </div>
+    <p class="text">로그인 하면 모든 서비스를 이용할 수 있어요!</p>
+  </div>
 
-    <div class="container">
-        <button @click="naverLogin" class="login-btn">
-            <img src="@/assets/images/naver_login.png" />
-        </button>
+  <div class="container">
+    <button @click="naverLogin" class="login-btn">
+      <img src="@/assets/images/naver_login.png" />
+    </button>
 
-        <button @click="googleLogin" class="login-btn">
-            <img src="@/assets/images/google_login.png" />
-        </button>
+    <button @click="googleLogin" class="login-btn">
+      <img src="@/assets/images/google_login.png" />
+    </button>
 
-        <button @click="kakaoLogin" class="login-btn">
-            <img src="@/assets/images/kakao_login.png" />
-        </button>
+    <button @click="kakaoLogin" class="login-btn">
+      <img src="@/assets/images/kakao_login.png" />
+    </button>
+  </div>
+
+  <a @click="infoModalOpen" href="#">
+    <p class="info">개인정보 처리방침</p>
+    <div class="modal-wrap" v-show="infoModal">
+      <div class="modal-container" @click.stop>
+        <infoPage @close="infoModalOpen" />
+      </div>
     </div>
-
-    <a @click="infoModalOpen" href="#">
-        <p class="info">개인정보 처리방침</p>
-        <div class="modal-wrap" v-show="infoModal">
-            <div class="modal-container" @click.stop>
-                <infoPage @close="infoModalOpen" />
-            </div>
-        </div>
-    </a>
+  </a>
 </template>
 
 <style scoped>
 .title {
-    display: flex;
-    justify-content: space-between;
-    margin: 0;
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
 }
 
 .login-title {
-    flex: 1;
-    text-align: center;
-    margin: 0;
-    padding-left: 30px;
+  flex: 1;
+  text-align: center;
+  margin-top: 10px;
+  color: #955a1f;
 }
 
 a {
-    color: black;
+  color: black;
 }
 
 .close-btn {
-    border: none;
-    background: none;
-    margin-right: 10px;
-    padding: 0px;
+  border: none;
+  background: none;
+  margin-right: 10px;
+  padding: 0px;
 }
 
 .text {
-    text-align: center;
-    margin: 20px 0px;
+  text-align: center;
+  margin: 20px 0px;
 }
 
 .info {
-    text-align: center;
-    margin: 20px 0px;
+  text-align: center;
+  margin: 20px 0px;
 }
 
 .container {
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-    align-items: center;
-    padding-top: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  align-items: center;
+  padding-top: 10px;
 }
 
 .login-btn {
-    border: none;
-    background: none;
-    padding: 0;
-    outline: none;
+  border: none;
+  background: none;
+  padding: 0;
+  outline: none;
 }
 
 .login-btn img {
-    width: 180px;
-    height: auto;
+  width: 180px;
+  height: auto;
 }
 
 .modal-wrap {
-    position: fixed;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 20;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 20;
 }
 
 .modal-container {
-    position: relative;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 300px;
-    height: 400px;
-    background: #fff;
-    border-radius: 10px;
-    padding: 20px;
-    box-sizing: border-box;
-    z-index: 20;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 400px;
+  background: #fff;
+  border-radius: 10px;
+  padding: 20px;
+  box-sizing: border-box;
+  z-index: 20;
 }
 </style>
